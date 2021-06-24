81a18b417d597a376565a78c036669ffb8fd44ac
Mariya Veleva <56577507+mariyaveleva16@users.noreply.github.com>
2021-06-24 18:04:42 +0300

    Add files via upload

---
+++ HW1/3/Header.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include <iostream>
+#include <cmath>
+class SVG
+{
+private:
+	double x, y;
+
+public:
+
+	SVG()
+	{
+		x = y = 0;
+	};
+	SVG(double, double);
+	void read();
+	double getX() const {
+		return x;
+	}
+	double getY() const
+	{
+		return y;
+	}
+	void print() const;
+	double lenght(SVG) const;
+	double height(SVG) const;
+	double s(SVG) const;
+
+	void setX(double x)
+	{
+		this->x = x;
+	}
+	void setY(double y)
+	{
+		this->y = y;
+	}
+
+};
---
+++ HW1/3/Source.cpp
@@ -0,0 +1,32 @@
+﻿
+#include "Header.h"
+
+SVG::SVG(double x, double y)
+{
+	this->x = x;
+	this->y = y;
+}
+void SVG::read()
+{
+	std::cin >> x;
+	std::cin >> y;
+}
+double SVG::lenght(SVG p) const
+{
+	return abs(x - p.x);
+}
+double SVG::height(SVG p) const
+{
+	return abs(y - p.y);
+}
+double SVG::s(SVG p) const
+{
+	return lenght(p) *height(p);
+}
+
+void SVG::print() const
+{
+	std::cout << '(' << x << ", " << y << ')';
+}
+
+
