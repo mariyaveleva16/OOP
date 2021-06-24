de4e3fcb99350c94cced1e9b889ba36c36a97571
Mariya Veleva <56577507+mariyaveleva16@users.noreply.github.com>
2021-06-24 18:04:28 +0300

    Create main.cpp

---
+++ HW1/3/main.cpp
@@ -0,0 +1,35 @@
+#include "Header.h"
+
+int main()
+{
+	int a;
+	std::cout << "Enter how many rectangles you want to have in the list: ";
+	std::cin >> a;
+	double b;
+	double* arr;
+	arr = new double[a + 1];
+	double max = -1;
+	int index = 0;
+	for (int i = 0; i < a; i++)
+	{
+		SVG p; SVG q;
+		p.read();
+		std::cout << "Rectangle " << i << " p";
+		p.print();
+		std::cout << std::endl;
+		q.read();
+		std::cout << "Rectangle " << i << " q";
+		q.print();
+		std::cout << std::endl;
+		b = p.s(q);
+		arr[i] = b;
+		if (max < arr[i])
+		{
+			max = arr[i];
+			index = i;
+		}
+	}
+	std::cout << "Index=" << index;
+
+	return 0;
+}
