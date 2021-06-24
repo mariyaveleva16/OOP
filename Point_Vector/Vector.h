dc235b9c3c37760a9f42f2187f2a1fd9a0be3578
Mariya Veleva <56577507+mariyaveleva16@users.noreply.github.com>
2021-06-24 22:01:21 +0300

    Add files via upload

---
+++ Point_Vector/Vector.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+/*
+* Vector.h
+*/
+
+#include "Ponit.h"
+
+#ifndef VECTOR_H
+#define VECTOR_H
+class Vector
+{
+private:
+	Point start;
+	Point end;
+public:
+	// конструктури
+	Vector(Point, Point);
+
+	// функции за достъп
+	Point getStart() const
+	{
+		return start;
+	}
+	Point getEnd() const
+	{
+		return end;
+	}
+	void print() const;
+	double length() const;
+
+	// мутатори
+	void setStart(Point a)
+	{
+		start = a;
+	}
+	void setEnd(Point b)
+	{
+		end = b;
+	}
+	void offset(double, double);
+};
+
+#endif // VECTOR_H
