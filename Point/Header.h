576fa0cfc300a88120a7138801c0ba183857e784
Mariya Veleva <56577507+mariyaveleva16@users.noreply.github.com>
2021-06-24 21:04:34 +0300

    Add files via upload

---
+++ Point/Header.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include <iostream>
+#include <cmath>
+class Point
+{
+private:
+	double x, y;
+
+public:
+
+	Point()
+	{
+		x = y = 0;
+	};
+	Point(double, double);
+	void read();
+	double getX() const {
+		return x;
+	}
+	double getY() const
+	{
+		return y;
+	}
+	void print() const;
+	double lenght(Point) const;
+	double height(Point) const;
+	double s(Point) const;
+
+	void setX(double x)
+	{
+		this->x = x;
+	}
+	void setY(double y)
+	{
+		this->y = y;
+	}
+
+};
---
+++ Point/Source.cpp
@@ -0,0 +1,32 @@
+﻿
+#include "Header.h"
+
+Point::Point(double x, double y)
+{
+	this->x = x;
+	this->y = y;
+}
+void Point ::read() 
+{
+	std::cin >> x;
+	std::cin >> y;
+}
+double Point::lenght(Point p) const
+{
+	return abs(x - p.x);
+}
+double Point::height(Point p) const
+{
+	return abs(y - p.y);
+}
+double Point::s(Point p) const
+{
+	return lenght(p) *height(p);
+}
+
+void Point::print() const
+{
+	std::cout << '(' << x << ", " << y << ')';
+}
+
+
